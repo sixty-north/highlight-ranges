6dde88819210337f526fe43046becb0e8495d868
Austin Bingham <austin.bingham@gmail.com>
2018-12-16 20:40:27 +0100

    Based black-on-red style example on pygment's default style.

--- src/highlight_ranges/styles.py
+++ src/highlight_ranges/styles.py
@@ -2,13 +2,15 @@
 """
 
 from pygments.style import Style
+from pygments.styles.default import DefaultStyle
 from pygments.token import Generic
 
 
 class BlackOnRed(Style):
     """An example style that highlights with black text on a red background.
+
+    It's based on the default pygments style.
     """
     default_style = ""
-    styles = {
-        Generic.Emph:           'bg:#f00 #fff'
-    }
+    styles = dict(DefaultStyle.styles)
+    styles[Generic.Emph] = 'bg:#f00 #000'
